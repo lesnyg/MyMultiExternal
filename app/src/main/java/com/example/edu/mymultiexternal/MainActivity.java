65567e1dfe42dd0305bd0ee84a370efbda2fab82
lesnyg <lsy79@hanmail.net>
2018-11-13 16:12:57 +0900

    commit1

--- app/src/main/java/com/example/edu/mymultiexternal/MainActivity.java
+++ app/src/main/java/com/example/edu/mymultiexternal/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.edu.mymultiexternal;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,7 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int LOAD_IMAGE = 101;
+    int IMAGE_CAPTURE = 102;
     InputStream inputStream = null;
 
     @Override
@@ -24,23 +27,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button fromGalleryButton = findViewById(R.id.fromGalleryButton);
         fromGalleryButton.setOnClickListener(this);
+        Button imageCaptureButton = findViewById(R.id.imageCaptureButton);
+        imageCaptureButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent,LOAD_IMAGE);
+        switch (v.getId()) {
+            case R.id.fromGalleryButton:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, LOAD_IMAGE);
+                break;
+            case R.id.imageCaptureButton:
+                if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+                    Intent intent1 = new Intent();
+                    intent1.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent1, IMAGE_CAPTURE);
+                    break;
+                }
+        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode,int resultCode,Intent data){
-
-        if(data != null){
-            ImageView imageViewFromGallery = findViewById(R.id.imageViewFromGallery);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView imageViewFromGallery = findViewById(R.id.imageViewFromGallery);
+        if (requestCode == IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            imageViewFromGallery.setImageBitmap(bitmap);
+        } else if (requestCode == LOAD_IMAGE && resultCode == RESULT_OK) {
             Uri selectedImage = data.getData();
-
 
             try {
                 inputStream = this.getContentResolver().openInputStream(selectedImage);
